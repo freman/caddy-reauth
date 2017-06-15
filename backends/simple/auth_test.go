57f1f810ee671a0d665f830b1684f18aaacd4095
Shannon Wynter <me@example.com>
2017-06-15 13:54:21 +1000

    More tests

---
+++ backends/simple/auth_test.go
@@ -0,0 +1,100 @@
+package simple
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestAuthenticate(t *testing.T) {
+	auth := &Simple{credentials: map[string]string{"bob-bcrypt": "secret"}}
+
+	t.Log("Testing no credentials")
+	r, _ := http.NewRequest("GET", "https://test.example.com", nil)
+	ok, err := auth.Authenticate(r)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if ok {
+		t.Error("Authenticate should have failed")
+	}
+
+	t.Log("Testing wrong credentials")
+	r.SetBasicAuth("fred", "blogs")
+	ok, err = auth.Authenticate(r)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if ok {
+		t.Error("Authenticate should have failed")
+	}
+
+	t.Log("Testing correct credentials")
+	r.SetBasicAuth("bob-bcrypt", "secret")
+	ok, err = auth.Authenticate(r)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if !ok {
+		t.Error("Authenticate should have succeeded")
+	}
+}
+
+func TestAuthenticateConstructor(t *testing.T) {
+	tests := []struct {
+		desc   string
+		config string
+		expect *Simple
+		err    error
+	}{
+		{
+			`Empty configuration`,
+			``,
+			nil,
+			errors.New(`backend configuration has to be in form 'key1=value1,key2=..', but was `),
+		}, {
+			`Test correct usage`,
+			`username=password`,
+			&Simple{credentials: map[string]string{"username": "password"}},
+			nil,
+		}, {
+			`Test multiple users`,
+			`username=password,bob=bcrypt`,
+			&Simple{credentials: map[string]string{"username": "password", "bob": "bcrypt"}},
+			nil,
+		}, {
+			`Test bad configuration`,
+			`username`,
+			nil,
+			errors.New(`backend configuration has to be in form 'key1=value1,key2=..', but was username`),
+		},
+	}
+
+	for i, tc := range tests {
+		t.Logf("Testing configuration %d (%s)", i+1, tc.desc)
+		be, err := constructor(tc.config)
+		if tc.err != nil {
+			if err == nil {
+				t.Error("Expected error, got none")
+			} else if err.Error() != tc.err.Error() {
+				t.Errorf("Expected `%v` got `%v`", tc.err, err)
+			}
+		} else if err != nil {
+			t.Errorf("Unexpected error `%v`", err)
+		}
+
+		if tc.expect == nil {
+			if be != nil {
+				t.Errorf("Expected nil rules, got %v", be)
+			}
+		} else {
+			actual, ok := be.(*Simple)
+			if !ok {
+				t.Errorf("Expected *Simple, got %T", be)
+			} else if !reflect.DeepEqual(tc.expect, actual) {
+				t.Errorf("Expected %v got %v", tc.expect, actual)
+			}
+		}
+	}
+}
---
+++ reauth_test.go
@@ -0,0 +1,97 @@
+package reauth
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mholt/caddy"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+)
+
+func emptyHandler(w http.ResponseWriter, r *http.Request) (int, error) {
+	return http.StatusOK, nil
+}
+
+func TestSetup(t *testing.T) {
+	test := `reauth {
+				path /test
+				simple username=password
+			}`
+
+	c := caddy.NewTestController("http", test)
+	err := setup(c)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+}
+
+func TestInvalidSetup(t *testing.T) {
+	test := `reauth {
+				path /test
+				simple usernamepassword
+			}`
+
+	c := caddy.NewTestController("http", test)
+	err := setup(c)
+	if err == nil {
+		t.Errorf("Expected error, got none")
+	}
+}
+
+func TestMiddlewareProvider(t *testing.T) {
+	test := `reauth {
+				path /test
+				except /test/foo
+				except /test/bar
+				simple username=password
+			}`
+	c := caddy.NewTestController("http", test)
+
+	rules, _ := parseConfiguration(c)
+
+	auth := &Reauth{
+		rules: rules,
+		next:  httpserver.HandlerFunc(emptyHandler),
+		realm: "test",
+	}
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	rec := httptest.NewRecorder()
+
+	result, err := auth.ServeHTTP(rec, req)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if result != http.StatusOK {
+		t.Errorf("Expected `%v` got `%v`", http.StatusOK, result)
+	}
+
+	req.URL.Path = "/test"
+	result, err = auth.ServeHTTP(rec, req)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if result != http.StatusUnauthorized {
+		t.Errorf("Expected `%v` got `%v`", http.StatusUnauthorized, result)
+	}
+
+	req.URL.Path = "/test/foo"
+	result, err = auth.ServeHTTP(rec, req)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if result != http.StatusOK {
+		t.Errorf("Expected `%v` got `%v`", http.StatusOK, result)
+	}
+
+	req.URL.Path = "/test"
+	req.SetBasicAuth("username", "password")
+	result, err = auth.ServeHTTP(rec, req)
+	if err != nil {
+		t.Errorf("Unexpected error `%v`", err)
+	}
+	if result != http.StatusOK {
+		t.Errorf("Expected `%v` got `%v`", http.StatusOK, result)
+	}
+}
